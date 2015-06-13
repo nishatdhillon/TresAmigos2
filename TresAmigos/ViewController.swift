81f01fa821dcbaf0c16c2380f2cad4a6087a4e58
David L'Heureux <davidlheureux@Davids-Air.dhcp.bngf.local>
2015-06-12 21:49:10 -0400

    Initial Commit

---
+++ TresAmigos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TresAmigos
+//
+//  Created by David L'Heureux on 2015-06-12.
+//  Copyright (c) 2015 David L'Heureux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
